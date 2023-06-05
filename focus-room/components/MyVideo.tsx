89e01f332dfdc3244d7d0e6362bc5eed7eb504f6
2210040 <2210040@ecc.ac.jp>
2023-06-05 14:41:44 +0900

    カメラとマイクの切り替えができるようになりました

--- focus-room/components/MyVideo.tsx
+++ focus-room/components/MyVideo.tsx
@@ -4,68 +4,65 @@ import { Switch, Card, CardMedia, Button } from '@mui/material';
 
 export default function MyVideo() {
   const videoRef: any = useRef(null); // ビデオのRefを作成(このRefを変えることで、映像を映したりすることができます)
-  const [cameravisible, setCameravisible] = useState(true);
-  const [volumevisible, setVolumevisible] = useState(true);
+  const [cameravisible, setCameravisible] = useState(false);
+  const [volumevisible, setVolumevisible] = useState(false);
 
-  function share_display(){
-    navigator.mediaDevices.getDisplayMedia({ audio: false, video: true })
-  }
-  
-  function switch_camera(e: any){
-    setCameravisible(!cameravisible)    
-    navigator.mediaDevices
-    .getUserMedia({
-      // ここで、選択したデバイスをセットしています。
-      audio: true,
-      video: {
-        width: {
-          min: 640,
-          max: 640,
-        },
-        height: {
-          min: 480,
-          max: 480,
-        }
-      }
-    })
-    .then((stream) => {
-      if(e.target.checked){
-        if (!videoRef?.current) return;
-      // 現在接続されているデバイスを使いストリーミングしています。
-      videoRef.current.srcObject = stream;
-      videoRef.current.volume = 0;
-      videoRef.current.play();
-      }else{
-        videoRef.current.srcObject = null;
-      }
-    })
-    .catch((e) => {
-      // エラー処理
-      alert()
-    });
-  }
+  // カメラを切り替えるための関数
+  async function switchCamera(e) {
+    if (e.target.checked) {
+      navigator.mediaDevices.getUserMedia({ video: { width: 300, height: 150 }, audio: false })
+        .then((stream) => {
+          // 取得した音声ストリームを利用するための処理を記述します
+          let cameraElemet: any = document.querySelector("#video")
+          cameraElemet.srcObject = stream
+          cameraElemet.play()
 
-  function switch_volume(e: any){
-    setVolumevisible(!volumevisible)
-    if(e.target.checked){
-      videoRef.current.volume = 0;
-    }else{
-      videoRef.current.volume = 1;
+        })
+        .catch((error) => {
+          // マイクへのアクセスが失敗した場合の処理
+          console.error('カメラへのアクセスが失敗しました。', error);
+        });
+    } else {
+      let cameraElemet: any = document.querySelector("#video")
+      cameraElemet.srcObject = null
     }
+  }
+
+  // マイクを切り替えるための関数
+  async function switchMicrophone(e) {
+    if (e.target.checked) {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then((stream) => {
+          // 取得した音声ストリームを利用するための処理を記述します
+          let cameraElemet: any = document.querySelector("#audio")
+          cameraElemet.srcObject = stream
+          cameraElemet.play()
+        })
+        .catch((error) => {
+          // マイクへのアクセスが失敗した場合の処理
+          console.error('マイクへのアクセスが失敗しました。', error);
+        });
+    } else {
+      let audioElemet: any = document.querySelector("#audio")
+      audioElemet.srcObject = null
+    }
+  }
+
+  function share_display() {
+    navigator.mediaDevices.getDisplayMedia({ audio: false, video: true })
   }
 
   return (
     <>
-        <Card>
-          <CardMedia>
-            <video ref={videoRef} id="video" height="240"></video>
-          </CardMedia>
-            {cameravisible ? <Icon.CameraVideoOff className="ms-3" width={20} height={20} /> : <Icon.CameraVideo className="ms-3" width={20} height={20} /> }
-            <Switch color="primary" onChange={(e) => switch_camera(e)}/>
-            {volumevisible ? <Icon.VolumeMuteFill width={20} height={20} /> : <Icon.VolumeUpFill width={20} height={20} />}
-            <Switch color="primary" onChange={(e) => switch_volume(e)}/>
-            <Button variant="text" onClick={() => share_display()}>画面共有</Button>
-        </Card>
+      <Card>
+        <video id="video" src=""></video>
+        <audio id="audio" src=""></audio>
+        {cameravisible ? <Icon.CameraVideo className="ms-3" width={20} height={20} /> : <Icon.CameraVideoOff className="ms-3" width={20} height={20} />}
+        <Switch color="primary" onChange={(e) => switchCamera(e)} />
+        {volumevisible ? <Icon.VolumeUpFill width={20} height={20} /> : <Icon.VolumeMuteFill width={20} height={20} />}
+        <Switch color="primary" onChange={(e) => switchMicrophone(e)} />
+        <Button variant="text" onClick={() => share_display()}>画面共有</Button>
+      </Card>
     </>
   );
 }
