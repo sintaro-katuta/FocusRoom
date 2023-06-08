74570e1bb69dc625a0cb4717b44c1611a41c8075
2210040 <2210040@ecc.ac.jp>
2023-06-08 09:55:23 +0900

    デザイン変えた

--- focus-room/src/pages/index.tsx
+++ focus-room/src/pages/index.tsx
@@ -17,8 +17,8 @@ const Home: NextPage = () => {
           <div className="col">
             <div id="primary-content" className="row">
               <h2 className="fw-light">Focus Room とは</h2>
-              <h3 className="mt-2 ms-3 fw-light">-どんな人向け？</h3>
-              <ul className="my-4 ms-4 list-group list-group-horizontal-sm">
+              <h3 className="mt-2 ms-3 fw-light">- どんな人向け？</h3>
+              <ul className="my-4 ms-2 me-2 list-group list-group-horizontal-sm">
                 <li className="list-group-item secondary-background-color">
                   集中して作業したいけど集中してできない😒
                 </li>
@@ -35,25 +35,25 @@ const Home: NextPage = () => {
 
               <Icon.ArrowDown className="my-2" width={30} height={30} />
 
-              <div className="ms-4 mt-3 border border-dark">
-                <h3 className="mt-2 fw-light text-center">
+              <div className="ms-3 mt-3 mb-4 border border-dark">
+                <h4 className="mt-4 fw-light text-center">
                   そんな方々に向けた<span className="fw-bold">超集中</span>
                   プラットフォーム
-                </h3>
-                <h3 className="mt-2 ms-4 py-3 py-2 fw-light text-center">
+                </h4>
+                <h3 className="mt-2 ms-3 py-3 py-2 fw-light text-center">
                   Focus Room
                 </h3>
               </div>
 
-              <h3 className="mt-2 ms-3 fw-light">-集中</h3>
-              <p className="mt-2 py-4 ms-4 h6 fw-light rounded secondary-background-color">
+              <h3 className="mt-2 ms-2 fw-light">- 集中</h3>
+              <p className="mt-2 py-4 ms-2 me-2 h6 fw-light rounded secondary-background-color">
                 集中して作業に取り組みたいとき一人でするよりも友達としたり、カフェでしたほうがはかどりませんか？
                 一人でも複数人でも集中できるそんな
                 <span className="fw-bold">部屋</span>を共有することができます
               </p>
 
-              <h3 className="mt-2 ms-3 fw-light">-部屋</h3>
-              <p className="mt-2 py-4 ms-4 h6 fw-light rounded secondary-background-color">
+              <h3 className="mt-2 ms-2 fw-light">- 部屋</h3>
+              <p className="mt-2 py-4 ms-2 me-2 h6 fw-light rounded secondary-background-color">
                 それぞれの部屋では音楽をかけたり、他の人と話しながら作業をできます。
                 部屋のリーダーがかける音楽を決めたり、制限をつける事ができます。
               </p>
